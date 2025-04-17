cd0ccf7fc65dfcfe3bbb5354645b19b2b599eb08
pradip2004 <pradipsadhukhan16@gmail.com>
2025-04-17 18:44:51 +0530

    Setup VAPI Ai and convex database

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+
+
+export default {
+      providers: [
+        {
+          domain: "https://musical-seasnail-75.clerk.accounts.dev/",
+          applicationID: "convex",
+        },
+      ]
+    };
